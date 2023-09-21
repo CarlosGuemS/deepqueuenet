86f5031658d474266d8e8c3a9e57ffe360f880fe
Carlos Güemes <carguemespa@gmail.com>
2023-09-21 07:28:09 +0000

    bug fixes for port agg

--- testbed/data_processing.py
+++ testbed/data_processing.py
@@ -48,14 +48,16 @@ def process_sample_total_agg(packet_info_matrix, split, sample_id):
     )
 
 
-def process_sample_port_agg(packet_info_matrix, split, sample_id):
+def process_sample_port_agg(packet_info_matrix, split, sample_id, exit_agg=False):
     # Process sequences
-    # We will later aggregate per EXIT port
+    # if exit_agg is true, use exit port
+    # if exit_agg is false, use entry port
     processed_sequences = [list() for _ in range(5)]
     for ii, jj in permutations(range(5), 2):
         if packet_info_matrix[ii][jj]:
             for packet_sequence in packet_info_matrix[ii][jj][0]:
-                processed_sequences[jj].append(
+                idx = jj if exit_agg else ii
+                processed_sequences[idx].append(
                     [
                         (pkt[0], pkt[1], ii, jj, pkt[2])
                         for pkt in packet_sequence
@@ -68,7 +70,7 @@ def process_sample_port_agg(packet_info_matrix, split, sample_id):
     ]
     # Transform ts from ns to s, set up initial time as 0
     global_initial_time = min(
-        merged_sequence[0] for merged_sequence in merged_sequences
+        merged_sequence[0][0] for merged_sequence in merged_sequences
     )
     merged_sequences = [
         [
@@ -79,7 +81,7 @@ def process_sample_port_agg(packet_info_matrix, split, sample_id):
     ]
 
     # Store as separate csv's
-    for jj, merged_sequence in merged_sequences:
+    for jj, merged_sequence in enumerate(merged_sequences):
         pd.DataFrame(
             {
                 "timestamp (sec)": [ts for ts, _, _, _, _ in merged_sequence],
@@ -90,7 +92,9 @@ def process_sample_port_agg(packet_info_matrix, split, sample_id):
                 "time_in_sys": [delay for _, _, _, _, delay in merged_sequence],
             }
         ).to_csv(
-            f"../data/5-port router/exit_port/_traces/{split}/5port_sample_{sample_id}_port_{jj}.csv",
+            "../data/5-port router/{}_port/_traces/{}/5port_sample{}_port{}.csv".format(
+                "exit" if exit_agg else "entry", split, sample_id, jj
+            ),
             index=False,
         )
 
@@ -101,6 +105,8 @@ if os.path.exists("../data/5-port router"):
     shutil.rmtree("../data/5-port router")
 os.makedirs("../data/5-port router/exit_port/_traces/_train")
 os.makedirs("../data/5-port router/exit_port/_traces/_test")
+os.makedirs("../data/5-port router/entry_port/_traces/_train")
+os.makedirs("../data/5-port router/entry_port/_traces/_test")
 os.makedirs("../data/5-port router/full_device/_traces/_train")
 os.makedirs("../data/5-port router/full_device/_traces/_test")
 print("Folder created")
@@ -136,10 +142,17 @@ with ProcessPoolExecutor(15) as executor:
             split,
             true_id,
         )
+        executor.submit(
+            process_sample_port_agg,
+            pkts_info,
+            split,
+            true_id,
+            True,
+        )
 # for raw_id, sample in enumerate(iter(ds)):
 #     split = "_train" if raw_id < train_length else "_test"
 #     print(sample.get_sample_id()[1])
-#     process_sample(
+#     process_sample_port_agg(
 #         sample.get_pkts_info_object(),
 #         split,
 #         sample.get_sample_id()[1],
